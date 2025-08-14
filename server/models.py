524363f05b02be1b4e5e9152b88383df53c1b484
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-14 16:11:18 -0600

    set up pattern and pattern_relationship models

--- server/models.py
+++ server/models.py
@@ -5,31 +5,46 @@ class Project(db.Model):
 	__tablename__ = "projects"
 
 	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.string, nullable=False)
-	status = db.Column(db.string, default="planning")
-	notes = db.Column(db.text, nullable=True)
+	title = db.Column(db.String, nullable=False)
+	status = db.Column(db.String, default="planning")
+	notes = db.Column(db.Text, nullable=True)
 	date_created = db.Column(db.Date, nullable=False)
 	date_updated = db.Column(db.Date, nullable=False)
 	
-	pattern_id = db.Column(db.Integer, db.ForeignKey('pattern.id'))
-	pattern = db.relationship("Pattern", back_poplulates="projects")
+	pattern_id = db.Column(db.Integer, db.ForeignKey('patterns.id'))
+	pattern = db.relationship("Pattern", back_populates="projects")
 
 	def __repr__(self):
 		return f'<Project {self.id}, {self.pattern_id}, {self.title}, {self.status}, {self.notes}, {self.date_created}, {self.date_updated}>'
 
 class Pattern(db.Model):
-	__tablename__ = "projects"
+	__tablename__ = "patterns"
 
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String, nullable=False)
+	brand = db.Column(db.String, nullable=False)
+	pattern_number = db.Column(db.String, nullable=True)
+	category = db.Column(db.String, nullable=False)
+	notes = db.Column(db.Text, nullable=True)
+	
 	def __repr__(self):
-		return f'<Project {self.id}>'
-
+		return f'<Pattern {self.id}, {self.name}, {self.brand}, {self.pattern_number}, {self.category}, {self.notes}>'
 
 class PatternRequirement(db.Model):
-	__tablename__ = "projects"
+	__tablename__ = "pattern_requirements"
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String, nullable=False)
+	brand = db.Column(db.String, nullable=False)
+	pattern_number = db.Column(db.string, nullable=True)
+	category = db.Column(db.String, nullable=False)
+	notes = db.Column(db.Text, nullable=True)
+
+	pattern_id = db.Column(db.Integer, db.ForeignKey('patterns.id'))
+	pattern = db.relationship("Pattern", back_populates="pattern_requirements")
 
 	def __repr__(self):
-		return f'<Project {self.id}>'
-
+		return f'<PatternRequirement {self.id}, {self.name}, {self.brand}, {self.pattern_number}, {self.category}, {self.notes}>'
 
 class Material(db.Model):
 	__tablename__ = "projects"
