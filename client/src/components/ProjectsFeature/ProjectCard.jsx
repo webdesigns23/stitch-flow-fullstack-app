3bb25e98320772679070e5642c827d55b47ddd61
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-18 19:16:13 -0600

    added date created and date updated to ProjectCard

--- client/src/components/ProjectsFeature/ProjectCard.jsx
+++ client/src/components/ProjectsFeature/ProjectCard.jsx
@@ -5,6 +5,8 @@ export default function ProjectCard({project, handleDelete}) {
 			<h2>{project.title}</h2>
 			<p>Status: "{project.status}"</p>
 			<p>Notes: {project.notes}</p>
+			<p>Created: {project.created_at}</p>
+			{project.updated_at && <p>Updated: {project.updated_at}</p>}
 			<button onClick={handleDelete}>Remove</button>
 		</>
 	)
