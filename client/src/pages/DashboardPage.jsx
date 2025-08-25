80fd6af0bbd04c67b30ab1a321bb084b48a3de84
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-08-25 10:28:36 -0600

    added materials list to dashboard

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import StatsGallery from "../components/DashboardFeature/StatsGallery";
+import PatternReqs from "../components/DashboardFeature/PatternReqs";
 
 export default function DashboardPage() {
 	return (
 	<> 
 	  <h1>Stitch Flow Dashboard</h1>
 	  <StatsGallery />
+	  <PatternReqs />
 	</>
   );
 }
